60a8e868095d2e4651a6b7e2c3aa2a04160f1c7c
shandylovecoding <shandylovecoding@gmail.com>
2021-07-05 10:57:49 +0800

    can link db to html

--- index.js
+++ index.js
@@ -2,35 +2,38 @@ const express = require('express')
 const upload = require('express-fileupload')
 const app = express();
 
+
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
 app.use(upload())
 
+app.use(express.static("public"))
+
 const db = []
 
+
 app.get('/',(req,res)=>{
-    res.sendFile(__dirname + "/pages/index.html")
+    res.sendFile(__dirname + "/index.html")
 })
 
 app.post('/upload', function(req, res) {
   let sampleFile;
   let uploadPath;
-
   if (!req.files || Object.keys(req.files).length === 0) {
     return res.status(400).send('No files were uploaded.');
   }
+  db.push(req.files.sampleFile.name)
 
-  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+  console.log(req.files);
+  // const file = new File(req.files)
   sampleFile = req.files.sampleFile;
   uploadPath = __dirname + '/uploads/' + sampleFile.name;
 
-  // Use the mv() method to place the file somewhere on your server
   sampleFile.mv(uploadPath, function(err) {
     if (err)
-      return res.status(500).send(err);
-
-    res.send('File uploaded!');
+      return res.status(500).send(err);  
   });
+  res.redirect('/')
 });
 
 app.listen(8000,()=>{
